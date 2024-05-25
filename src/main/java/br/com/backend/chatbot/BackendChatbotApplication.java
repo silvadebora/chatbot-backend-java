8baa9c805bbdd62c38a01fb8fbef620cbc92117a
Debora <silvadeborafisio@gmail.com>
2024-05-25 15:47:29 -0300

    feat:initial commit

---
+++ src/main/java/br/com/backend/chatbot/BackendChatbotApplication.java
@@ -0,0 +1,13 @@
+package br.com.backend.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backend/chatbot/BackendChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backend.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
